cd6deacb20a7d7104ba55f9118330965e8d1e336
Edeman George <71378676+eds-101@users.noreply.github.com>
2021-12-30 14:34:10 +0000

    sign promise

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -22,4 +22,33 @@ async function mintNFT(tokenURI) {
       .createNFT("0x0d28235B6191a66A3410cc1e3CeBfE53602D7865", tokenURI)
       .encodeABI(), // call the createNFT function from our OsunRiverNFT.sol file and pass the account that should receive the minted NFT.
   };
+  
+  const signPromise = alchemyWeb3.eth.accounts.signTransaction(
+    tx,
+    METAMASK_PRIVATE_KEY
+  );
+  signPromise
+    .then((signedTx) => {
+      alchemyWeb3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of our transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of our transaction!"
+            );
+          } else {
+            console.log(
+              "Something went wrong when submitting our transaction:",
+              err
+            );
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(" Promise failed:", err);
+    });
+
 }
