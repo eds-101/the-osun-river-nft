9ebd58aa5aaa01703bd939b1faed72c8f6f94249
Edeman George <71378676+eds-101@users.noreply.github.com>
2021-12-30 14:24:55 +0000

    contract data added

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -3,3 +3,5 @@ const API_URL = process.env.API_URL;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const alchemyWeb3 = createAlchemyWeb3(API_URL);
 const contract = require("../artifacts/contracts/OsunRiverNFT.sol/TorNFT.json"); 
+const contractAddress = "0x123b2ed2e798b43315161f6b59ca1879e608ab2f";
+const nftContract = new alchemyWeb3.eth.Contract(contract.abi, contractAddress);
