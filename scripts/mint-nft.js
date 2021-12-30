746c16b626452470f5dacef6db63af4e9e32dd27
Edeman George <71378676+eds-101@users.noreply.github.com>
2021-12-30 14:31:37 +0000

    mint nft function

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -5,3 +5,21 @@ const alchemyWeb3 = createAlchemyWeb3(API_URL);
 const contract = require("../artifacts/contracts/OsunRiverNFT.sol/TorNFT.json"); 
 const contractAddress = "0x123b2ed2e798b43315161f6b59ca1879e608ab2f";
 const nftContract = new alchemyWeb3.eth.Contract(contract.abi, contractAddress);
+
+const METAMASK_PUBLIC_KEY = process.env.METAMASK_PUBLIC_KEY;
+const METAMASK_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
+
+async function mintNFT(tokenURI) {
+    // get the nonce - nonce is needed for security reasons. It keeps track of the number of
+    // transactions sent from our address and prevents replay attacks.
+  const nonce = await alchemyWeb3.eth.getTransactionCount(METAMASK_PUBLIC_KEY, 'latest');
+  const tx = {
+    from: METAMASK_PUBLIC_KEY, // our MetaMask public key
+    to: contractAddress, // the smart contract address we want to interact with
+    nonce: nonce, // nonce with the no of transactions from our account
+    gas: 1000000, // fee estimate to complete the transaction
+    data: nftContract.methods
+      .createNFT("0x0d28235B6191a66A3410cc1e3CeBfE53602D7865", tokenURI)
+      .encodeABI(), // call the createNFT function from our OsunRiverNFT.sol file and pass the account that should receive the minted NFT.
+  };
+}
